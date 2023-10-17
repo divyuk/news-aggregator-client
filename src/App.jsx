63cda12b4e7a8cefc519068a4016013bc595addf
divyuk <div14kes@gmail.com>
2023-10-17 14:15:43 +0530

    Changning the route to news to updatePreferences

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from "./pages/Login";
 import { AuthProvider } from "./contexts/AuthenticationContext";
 import AppLayout from "./pages/AppLayout";
 import NewsList from "./components/NewsList";
+import UserPreferences from "./pages/UserPreferences";
 
 function App() {
   return (
@@ -15,8 +16,12 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/app" element={<AppLayout />}>
-            <Route index element={<Navigate replace to="news" />} />
+            <Route
+              index
+              element={<Navigate replace to="updatePreferences" />}
+            />
             <Route path="news" element={<NewsList />} />
+            <Route path="updatePreferences" element={<UserPreferences />} />
           </Route>
         </Routes>
       </BrowserRouter>
