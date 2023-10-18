769e45b38dd605dcf59d20181575caeebea51457
divyuk <div14kes@gmail.com>
2023-10-18 18:22:08 +0530

    Addding Search based on the Preferences

--- src/components/SelectionList.jsx
+++ src/components/SelectionList.jsx
@@ -1,27 +1,39 @@
+import { useState } from "react";
 import styles from "./SelectionList.module.css";
 
 function SelectionList({ items, maxSelection, title, selected, onSelect }) {
+  const [searchTerm, setSearchTerm] = useState(""); // State to track the search term
+
   const isItemSelected = (item) => selected.includes(item);
   const canSelectMore = selected.length < maxSelection;
 
   const toggleItem = (item) => {
     if (isItemSelected(item)) {
-      // Remove the item from the selection
       const updatedSelection = selected.filter(
         (selectedItem) => selectedItem !== item
       );
       onSelect(updatedSelection);
     } else if (canSelectMore) {
-      // Add the item to the selection if not at the maximum selection limit
       onSelect([...selected, item]);
     }
   };
 
+  // Filter items based on the search term
+  const filteredItems = items.filter((item) =>
+    item.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div>
       <h2>{title}</h2>
+      <input
+        type="text"
+        placeholder={`Search ${title}`}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
       <div className={styles.container}>
-        {items.map((item, index) => (
+        {filteredItems.map((item, index) => (
           <button
             key={index}
             className={`${styles.button} ${
