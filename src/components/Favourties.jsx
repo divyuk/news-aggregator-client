2b800e22d06ef8ef72711f4c82e05cf9d7b15be3
divyuk <div14kes@gmail.com>
2023-10-20 16:22:58 +0530

    Favourite component

--- src/components/Favourties.jsx
+++ src/components/Favourties.jsx
@@ -1,5 +1,31 @@
+import styles from "./Favourites.module.css";
+import { useEffect, useState } from "react";
+import { useAuth } from "../contexts/AuthenticationContext";
+import NewsCard from "./NewsCard";
+
 function Favourties() {
-  return <div>This is favourite Page</div>;
+  const [newsData, setNewsData] = useState([]);
+  const { getMyFavourites } = useAuth();
+
+  useEffect(() => {
+    async function getNewsData() {
+      try {
+        const data = await getMyFavourites();
+        setNewsData(data);
+      } catch (error) {
+        console.log("Error in fetching", error);
+      }
+    }
+    getNewsData();
+  }, [getMyFavourites]);
+
+  return (
+    <ul className={styles.newsList}>
+      {newsData.map((news, index) => (
+        <NewsCard key={index} news={news} />
+      ))}
+    </ul>
+  );
 }
 
 export default Favourties;
