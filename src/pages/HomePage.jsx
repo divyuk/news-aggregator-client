ec41c2cc365773a12b0fc540e84a3e66b1c1bfa9
divyuk <div14kes@gmail.com>
2023-10-20 21:34:12 +0530

    Home Page styles classes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,17 @@
 import PageNav from "../components/PageNav";
+import styles from "./HomePage.module.css";
 
 function HomePage() {
   return (
-    <main>
-      <section>
+    <main className={styles.homepage}>
+      <section className={styles.pageNav}>
         <PageNav />
       </section>
-      <section>
-        <h1>Welcome to News App</h1>
-        <p>This is where you know Gossips of the World 📃</p>
+      <section className={styles.content}>
+        <h1 className={styles.title}>Welcome to News App</h1>
+        <p className={styles.description}>
+          This is where you discover Gossips of the World 📃
+        </p>
       </section>
     </main>
   );
