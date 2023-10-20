c6341250f2f3f64f6227b4ca5fce9aaeb695fed9
divyuk <div14kes@gmail.com>
2023-10-20 14:29:38 +0530

    user account Info which has dp and dropwdown

---
+++ src/components/UserAccountInfo.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import styles from "./UserAccountInfo.module.css";
+import { Link } from "react-router-dom";
+
+function UserAccountInfo() {
+  const [dropdown, setDropDown] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropDown(!dropdown);
+  };
+
+  return (
+    <>
+      <div className={styles.container}>
+        <img
+          src="/user2.png"
+          alt="user display profile"
+          className={styles.dp}
+          onClick={toggleDropdown}
+        />
+        {dropdown && (
+          <div className={styles.dropdownContent}>
+            <Link to="favourite" className={styles.link}>
+              Favourite
+            </Link>
+            <Link to="read" className={styles.link}>
+              Read
+            </Link>
+            <Link to="settings" className={styles.link}>
+              Settings
+            </Link>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default UserAccountInfo;
