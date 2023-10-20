aa350cbf00d692bee3f89b4b533092661f831551
divyuk <div14kes@gmail.com>
2023-10-20 21:34:34 +0530

    Styles for Login page 💠

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useAuth } from "../contexts/AuthenticationContext";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import styles from "./Login.module.css";
 
 function Login() {
   const { login, isAuthenticated } = useAuth();
@@ -18,25 +19,34 @@ function Login() {
     if (isAuthenticated) navigate("/app/news", { replace: true });
   }, [isAuthenticated, navigate]);
   return (
-    <main>
+    <main className={styles["login-page"]}>
       <ToastContainer />
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email</label>
+      <form onSubmit={handleSubmit} className={styles["login-form"]}>
+        <h2 className={styles["login-title"]}>Login</h2>
+        <label htmlFor="email" className={styles["login-label"]}>
+          Email
+        </label>
         <input
           id="email"
           type="email"
           value={emailID}
           onChange={(e) => setEmailID(e.target.value)}
+          className={styles["login-input"]}
         />
 
-        <label htmlFor="password">Password</label>
+        <label htmlFor="password" className={styles["login-label"]}>
+          Password
+        </label>
         <input
           id="password"
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          className={styles["login-input"]}
         />
-        <button type="submit">Submit</button>
+        <button type="submit" className={styles["login-button"]}>
+          Submit
+        </button>
       </form>
     </main>
   );
