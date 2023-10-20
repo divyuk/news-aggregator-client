69e0289b57eac0a5379939f2c7be460acae43adb
divyuk <div14kes@gmail.com>
2023-10-20 14:29:07 +0530

    Adding user Account Info Component for Header Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,14 @@ import Heading from "./Heading";
 import Logo from "./Logo";
 import styles from "./Header.module.css";
 import { Outlet } from "react-router-dom";
-import Preferences from "./Preferences";
+import UserAccountInfo from "./UserAccountInfo";
 function Header() {
   return (
     <>
       <div className={styles.header}>
         <Logo />
         <Heading />
+        <UserAccountInfo />
       </div>
       {/* <Preferences /> */}
       <Outlet />
