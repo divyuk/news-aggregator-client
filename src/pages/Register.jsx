d39fbb287364d1cc7a10c37d00165edf580672fd
divyuk <div14kes@gmail.com>
2023-10-22 15:54:41 +0530

    Resieter with name and username update

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,53 +2,79 @@ import { useEffect, useState } from "react";
 import { useAuth } from "../contexts/AuthenticationContext";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import styles from "./Register.module.css";
 
 function Register() {
   const { register, isAuthenticated } = useAuth();
   const [emailID, setEmailID] = useState("");
   const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+
   const navigate = useNavigate();
 
+  const handleUsernameChange = (e) => {
+    const inputValue = e.target.value;
+    if (/^[A-Za-z0-9._]*$/.test(inputValue)) {
+      setUsername(inputValue);
+    }
+  };
   function handleSubmit(e) {
     e.preventDefault();
-    if (emailID && password) register(emailID, password);
+    if (emailID && password && name && username)
+      register(emailID, password, name, username);
   }
 
-  // useEffect(() => {
-  //   if (isAuthenticated) navigate("/", { replace: true });
-  // }, [isAuthenticated, navigate]);
-
-  // Performing a hard reload also
   useEffect(() => {
     if (isAuthenticated) {
       // Navigate to the homepage
       navigate("/app");
-
-      // // Perform a hard reload of the homepage
-      // window.location.reload(true);
     }
   }, [isAuthenticated, navigate]);
 
   return (
-    <main>
+    <main className={styles["register-page"]}>
       <ToastContainer />
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email</label>
+      <form onSubmit={handleSubmit} className={styles["register-form"]}>
+        <h2 className={styles["register-title"]}>One News</h2>
+        <p className={styles["tag-line"]}>
+          Regiter to see what&apos;s happeing around the world
+        </p>
         <input
           id="email"
           type="email"
           value={emailID}
           onChange={(e) => setEmailID(e.target.value)}
+          className={styles["register-input"]}
+          placeholder="Email"
         />
-
-        <label htmlFor="password">Password</label>
+        <input
+          id="name"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          className={styles["register-input"]}
+          placeholder="Full Name"
+        />
+        <input
+          id="username"
+          type="text"
+          value={username}
+          onChange={handleUsernameChange}
+          className={styles["register-input"]}
+          placeholder="Username"
+        />
         <input
           id="password"
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          className={styles["register-input"]}
+          placeholder="Password"
         />
-        <button type="submit">Submit</button>
+        <button type="submit" className={styles["register-button"]}>
+          Submit
+        </button>
       </form>
     </main>
   );
