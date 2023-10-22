f1e3dae873c4c14cc10c2eecb5dc5345907429f2
divyuk <div14kes@gmail.com>
2023-10-22 12:55:17 +0530

    Use state for page and handle Infinite Loading ▶

--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -1,25 +1,49 @@
 import styles from "./NewsList.module.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useAuth } from "../contexts/AuthenticationContext";
 import NewsCard from "./NewsCard";
 
 function NewsList() {
   const [newsData, setNewsData] = useState([]);
+  const [page, setPage] = useState(null);
   const { fetchNews } = useAuth();
+  const dataRef = useRef(null);
   useEffect(() => {
     async function getNewsData() {
       try {
-        const data = await fetchNews();
-        setNewsData(data);
+        const data = await fetchNews(page);
+        dataRef.current = data;
+        setNewsData(data.results);
       } catch (error) {
         console.log("Error in fetching", error);
       }
     }
     getNewsData();
-  }, [fetchNews]);
+  }, [fetchNews, page]);
+
+  const handleInfiniteScroll = () => {
+    try {
+      if (
+        window.innerHeight + document.documentElement.scrollTop + 3 >=
+        document.documentElement.scrollHeight
+      )
+        setPage(dataRef.current.nextPage);
+    } catch (error) {
+      console.log("Error while scrolling", error);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleInfiniteScroll);
+    return () => {
+      window.removeEventListener("scroll", handleInfiniteScroll);
+    };
+  }, []);
+
   if (newsData.length == 0) {
     return <h1>Nothing Found</h1>;
   }
+
   return (
     <ul className={styles.newsList}>
       {newsData.map((news, index) => (
